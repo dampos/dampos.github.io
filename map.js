445283911d1541472128eb53311ec3b97fb29501
dampos <thedamjan3@hotmail.com>
2016-05-26 00:24:55 +0200

    PopUp

--- map.js
+++ map.js
@@ -6,9 +6,9 @@ var coffee = [
   ['Birokrat', 41.995154, 21.435238, 1],
   ['Broz', 42.000039, 21.417306, 1],
   ['Belier', 41.998513, 21.417045, 1],
-  ['Coffee Factory', 41.999164, 21.423381,1],
+  ['CoffeeFactory', 41.999164, 21.423381,1],
   ['Stobi', 41.995274, 21.434044,1],
-  ['The Galery', 42.003866, 21.414994,1],
+  ['TheGalery', 42.003866, 21.414994,1],
 ];
 
 var restaurant =
@@ -25,10 +25,10 @@ var restaurant =
 var hotel =
 [
   ['Alexandar Palace', 42.010756, 21.405815,1],
-  ['Hotel Russia', 41.992868, 21.465447,1],
-  ['Hotel Arka', 42.001670, 21.438769,1],
-  ['Bushi Resort & Spa', 42.001700, 21.436238,1],
-  ['Holiday Inn', 41.994561, 21.436392,1],
+  ['HotelRussia', 41.992868, 21.465447,1],
+  ['HotelArka', 42.001670, 21.438769,1],
+  ['BushiResort', 42.001700, 21.436238,1],
+  ['HolidayInn', 41.994561, 21.436392,1],
 ];
 
 
@@ -36,68 +36,67 @@ var landscape =
 [
   ['Canyon Matka', 41.950804, 21.298937,1],
   ['Kale', 42.000353, 21.433479,1],
-  ['Vodno Millenium Cross', 41.965136, 21.394394,1],
-  ['Monastery of St. Mark', 41.893523, 21.402328,1],
-  ['', ,1],
+  ['VodnoMilleniumCross', 41.965136, 21.394394,1],
+  ['MonasteryofStMark', 41.893523, 21.402328,1],
 ];
 
 var shopping =
 [
   ['GTC', 41.994641,21.434718,1],
-  ['TC Soravija', 41.993860,21.435533,1],
-  ['Skopje City Mall', 42.004248,21.391763,1],
-  ['Ramstore Mall', 41.991756,21.427422,1],
+  ['TCSoravija', 41.993860,21.435533,1],
+  ['SkopjeCityMall', 42.004248,21.391763,1],
+  ['RamstoreMall', 41.991756,21.427422,1],
   ['Zebra', 41.992645, 21.419904,1],
 ];
 
 var museum =
 [
   ['Arheoloshki', 41.996842, 21.434226, 1],
-  ['Mak Borba', 41.998060,21.433229,1],
-  ['Muzej na SK', 41.991025,21.429197,1],
+  ['MakBorba', 41.998060,21.433229,1],
+  ['MuzejNaSK', 41.991025,21.429197,1],
   ['Holocaust', 41.998452,21.433501,1],
 ];
 
 var square =
 [
-  ['Macedonia Square', 41.995920,21.431483, 1],
-  ['Mother Teresa', 41.996669,21.435817,1],
+  ['MacedoniaSquare', 41.995920,21.431483, 1],
+  ['MotherTeresa', 41.996669,21.435817,1],
   ['Jadran', 41.996452,21.430082,1],
   ['Bazar', 42.001836,21.435915,1],
-  ['Filip II', 41.998025,21.434487,1],
+  ['FilipII', 41.998025,21.434487,1],
 ];
 
 
 var hangout =
 [
   ['Sektor', 41.995838, 21.424530, 1],
-  ['XL Club', 42.007105, 21.421495,1],
+  ['XLClub', 42.007105, 21.421495,1],
   ['Karting', 42.008585, 21.403677,1],
   ['Irish Pub', 41.995062, 21.435664,1],
 ];
 
 var historical =
 [
-  ['Gate Macedonia', 41.994446, 21.432508,1],
-  ['Stara Zeleznicka', 41.991395, 21.428804,1],
+  ['GateMacedonia', 41.994446, 21.432508,1],
+  ['StaraZeleznicka', 41.991395, 21.428804,1],
   ['Skupi', 42.016756, 21.393099,1],
-  ['House of Mother Teresa', 41.993802, 21.430823,1],
-  ['Stone Bridge', 41.996837, 21.432858,1],
+  ['HouseofMotherTeresa', 41.993802, 21.430823,1],
+  ['StoneBridge', 41.996837, 21.432858,1],
 ];
 
 
 var markers = [];
 
 var icons = [ 
-"http://dampos.github.io/images/coffee.png", 
-"http://dampos.github.io/images/restaurant.png",
-"http://dampos.github.io/images/hotel.png",
-"http://dampos.github.io/images/landscape.png",
-"http://dampos.github.io/images/shopping.png",
-"http://dampos.github.io/images/museum.png",
-"http://dampos.github.io/images/square.png",
-"http://dampos.github.io/images/hangout.png",
-"http://dampos.github.io/images/historic.png",
+"http://127.0.0.1/skp/images/coffee.png", 
+"http://127.0.0.1/skp/images/restaurant.png",
+"http://127.0.0.1/skp/images/hotel.png",
+"http://127.0.0.1/skp/images/landscape.png",
+"http://127.0.0.1/skp/images/shopping.png",
+"http://127.0.0.1/skp/images/museum.png",
+"http://127.0.0.1/skp/images/square.png",
+"http://127.0.0.1/skp/images/hangout.png",
+"http://127.0.0.1/skp/images/historic.png",
 ];
 
 var locations = [ 
@@ -105,9 +104,9 @@ var locations = [
   ['Birokrat', 41.995154, 21.435238, 1],
   ['Broz', 42.000039, 21.417306, 1],
   ['Belier', 41.998513, 21.417045, 1],
-  ['Coffee Factory', 41.999164, 21.423381,1],
+  ['CoffeeFactory', 41.999164, 21.423381,1],
   ['Stobi', 41.995274, 21.434044,1],
-  ['The Galery', 42.003866, 21.414994,1],
+  ['TheGalery', 42.003866, 21.414994,1],
 ],
 
 [
@@ -121,57 +120,57 @@ var locations = [
 ],
 
 [
-  ['Alexandar Palace', 42.010756, 21.405815,3],
-  ['Hotel Russia', 41.992868, 21.465447,3],
-  ['Hotel Arka', 42.001670, 21.438769,3],
-  ['Bushi Resort & Spa', 42.001700, 21.436238,3],
-  ['Holiday Inn', 41.994561, 21.436392,3],
+  ['AlexandarPalace', 42.010756, 21.405815,3],
+  ['HotelRussia', 41.992868, 21.465447,3],
+  ['HotelArka', 42.001670, 21.438769,3],
+  ['BushiResort', 42.001700, 21.436238,3],
+  ['HolidayInn', 41.994561, 21.436392,3],
 ],
 
 [
-  ['Canyon Matka', 41.950804, 21.298937,4],
+  ['CanyonMatka', 41.950804, 21.298937,4],
   ['Kale', 42.000353, 21.433479,4],
-  ['Vodno Millenium Cross', 41.965136, 21.394394,4],
-  ['Monastery of St. Mark', 41.893523, 21.402328,4],
+  ['VodnoMilleniumCross', 41.965136, 21.394394,4],
+  ['MonasteryofStMark', 41.893523, 21.402328,4],
 ],
 
 [
   ['GTC', 41.994641,21.434718,5],
-  ['TC Soravija', 41.993860,21.435533,5],
-  ['Skopje City Mall', 42.004248,21.391763,5],
-  ['Ramstore Mall', 41.991756,21.427422,5],
+  ['TCSoravija', 41.993860,21.435533,5],
+  ['SkopjeCityMall', 42.004248,21.391763,5],
+  ['RamstoreMall', 41.991756,21.427422,5],
   ['Zebra', 41.992645, 21.419904,5],
 ],
 
 
 [
   ['Arheoloshki', 41.996842, 21.434226, 6],
-  ['Mak Borba', 41.998060,21.433229,6],
-  ['Muzej na SK', 41.991025,21.429197,6],
+  ['MakBorba', 41.998060,21.433229,6],
+  ['MuzejNaSK', 41.991025,21.429197,6],
   ['Holocaust', 41.998452,21.433501,6],
 ],
 
 [
-  ['Macedonia Square', 41.995920,21.431483, 7],
-  ['Mother Teresa', 41.996669,21.435817,7],
+  ['MacedoniaSquare', 41.995920,21.431483, 7],
+  ['MotherTeresa', 41.996669,21.435817,7],
   ['Jadran', 41.996452,21.430082,7],
   ['Bazar', 42.001836,21.435915,7],
-  ['Filip II', 41.998025,21.434487,7],
+  ['FilipII', 41.998025,21.434487,7],
 ],
 
 [
   ['Sektor', 41.995838, 21.424530, 8],
-  ['XL Club', 42.007105, 21.421495,8],
+  ['XLClub', 42.007105, 21.421495,8],
   ['Karting', 42.008585, 21.403677,8],
   ['Irish Pub', 41.995062, 21.435664,8],
 ],
 
 [
-  ['Gate Macedonia', 41.994446, 21.432508,9],
-  ['Stara Zeleznicka', 41.991395, 21.428804,9],
+  ['GateMacedonia', 41.994446, 21.432508,9],
+  ['StaraZeleznicka', 41.991395, 21.428804,9],
   ['Skupi', 42.016756, 21.393099,9],
-  ['House of Mother Teresa', 41.993802, 21.430823,9],
-  ['Stone Bridge', 41.996837, 21.432858,9],
+  ['HouseofMotherTeresa', 41.993802, 21.430823,9],
+  ['StoneBridge', 41.996837, 21.432858,9],
 ],
 
 
@@ -196,7 +195,7 @@ var locations = [
         map.setMapTypeId(customMapTypeId);
 
 		
-    initialize();
+        initialize();
 		
 
       }
@@ -206,6 +205,10 @@ var locations = [
 
 function initialize()
 {
+
+  var contentString = '<iframe src=""></iframe>';
+  
+
   
   for(var i =0; i < locations.length; i++)
   {
@@ -227,7 +230,10 @@ function initialize()
         position: {lat: locations[i][x][1], lng: locations[i][x][2]},
         map: null,
         icon: image,
+        title: locations[i][x][0],
       });
+
+      addWindow(marker, '<iframe src="dsc/'+locations[i][x][0]+'.html"></iframe>');
 
       group.push(marker);
     }
@@ -241,6 +247,18 @@ function setMarkers(i) {
   {
     markers[i][x].setMap(map);
   }
+}
+
+function addWindow(marker, message)
+{
+
+  var infoWindow = new google.maps.InfoWindow({
+                content: message
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.open(map, marker);
+            });
 }
 
 function removeMarkers(i) {
